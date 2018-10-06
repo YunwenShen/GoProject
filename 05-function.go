1bb1ca4c551b335f44bdf1a913b8ba9b35c0b21d
shenyw <shenyw@citycloud.com.cn>
2018-10-06 11:28:45 +0800

    go 可变参数

--- 05-function.go
+++ 05-function.go
@@ -1,9 +1,14 @@
 package main
 
-import "math"
+import (
+	"math"
+	"fmt"
+)
 
 func main() {
-
+	welcome := []string{"hello", "world"}
+	change(welcome...)
+	fmt.Println(welcome)
 }
 
 // 函数示例
@@ -15,11 +20,24 @@ func function(param string) (int, int) {
 	return 0, 0
 }
 
+func myAbs(param int) (int, float64) {
+	return param, math.Abs(float64(param))
+}
+
 func test() {
 	// _空白符表示不接受该值
 	a, _ := myAbs(1)
+	fmt.Println(a)
 }
 
-func myAbs(param int) (int, float64) {
-	return param, math.Abs(float64(param))
+func find(num int, nums ...int) {
+	// 可变参数函数
+	fmt.Print(num, len(nums))
+}
+
+func change(s ...string) {
+	s[0] = "Go"
+	// append 会创建一个新的内存地址来存放append之后的值
+	a := append(s, "playground")
+	fmt.Println(a)
 }
